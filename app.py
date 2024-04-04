99741f59d824f52c0be9ff48f2bbcd864796072a
Miracle-Aligner <hellomax42@gmail.com>
2024-04-04 04:04:43 +0300

    added presentations page

--- app.py
+++ app.py
@@ -2,13 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def uk():
+    return render_template('index.html')
+
 @app.route("/en")
 def en():
     return render_template('index_en.html')
 
-@app.route("/")
-def uk():
-    return render_template('index.html')
+@app.route("/projects")
+def projects_uk():
+    return render_template('projects.html')
+
+@app.route("/en/projects")
+def projects_en():
+    return render_template('projects_en.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=7777)
